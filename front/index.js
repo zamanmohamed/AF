34befcf5d7489f4c8e645c7c0b053266fb3f0735
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2021-06-25 19:46:39 +0530

    mdb react
    mdb dependency install & Push

--- front/index.js
+++ front/index.js
@@ -1,4 +1,10 @@
 import React from "react";
+
+//import mdb react
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./src/App";
